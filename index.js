edca9cf0f36f0074309d045ff1413694ef80846a
UHK01 <90546824+UHK01@users.noreply.github.com>
2024-01-11 09:41:48 +0900

    scriptversionup

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.getElementById("ver").innerHTML = "4.0";
+document.getElementById("ver").innerHTML = "4.1";
 document.getElementById('hour').addEventListener('change', () => {
     input_check('hour');
 });
